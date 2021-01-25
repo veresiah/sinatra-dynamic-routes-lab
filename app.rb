0d111b85d84e40b93ae0858c8c56f13b8929568b
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-01-25 12:01:19 -0500

    commit tree 6d93cb147f039c85e44a5820518134a952de35b8

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do 
+    params[:name].reverse
+  end
 
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end 
+
+  get '/say/:number/:phrase' do 
+    output = ''
+    @phrase = params[:phrase]
+    params[:number].to_i.times do
+      output += params[:phrase]
+    end 
+    output
+  end 
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end 
+
+  get '/:operation/:number1/:number2' do 
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    output = "Cannot perform this operation"
+    case params[:operation]
+    when 'add'
+      output = (num1 + num2).to_s
+    when 'subtract'
+      output = (num1 - num2).to_s
+    when 'multiply'
+      output = (num1 * num2).to_s
+    when 'divide'
+      output = (num1 / num2).to_s 
+    end 
+  end 
 end
